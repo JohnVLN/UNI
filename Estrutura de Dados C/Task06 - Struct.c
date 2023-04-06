4f8eacf9d437d49377e4c944f87183d3e6584768
JoonathanVL <106608808+JohnVLN@users.noreply.github.com>
2023-04-06 14:54:43 -0300

    Update Task06 - Struct.c
    Corrigido erro onde não era calculado o saldo (float s) e o resultado exibido no final era sempre 0.00.

--- Estrutura de Dados C/Task06 - Struct.c
+++ Estrutura de Dados C/Task06 - Struct.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
 /* Função calculo de dispesa */
-float dis(float s, float val){
-    s = s - val;
+float dis(float s, float val) {
+    float res;
+    res = s - val;
+    return res;
 }
 
 /* Funcção calculo de receita */
 float rec(float s, float val) {
-    s = s + val;
+    float res;
+    res = s + val;
+    return res;
 }
 
 /* struct de projeto */
@@ -17,7 +21,7 @@ struct projeto {
 } p;
 
 int main() {
-    float s, val; /* float de saldo e valor de recurso */
+    float s, val, initial_s; /* float de saldo e valor de recurso */
     char type; /* tipo de despesa */
     int a; /* numero de recursos a serem calculados */
     int i = 0;
@@ -26,30 +30,29 @@ int main() {
     scanf("%d", &p.num);
     printf("Insert project value: ");
     scanf("%f", &p.s);
+    initial_s = p.s;
     
-    
-    /*  */
     printf("-------------------------------------------\n");
-    printf("Recursos: Insira os valores a serem somados \nou subtraídos do saldo do projeto.\n");
+    printf("Recursos:\n");
     printf("Quantos recursos serão calculados? ");
     scanf("%d", &a);
     
+    /* calculo de saldo */
     for(i = 0; i < a; i++) {
         printf("Valor do recurso: ");
         scanf("%f", &val);
         printf("r (receita) ou d (dispensa)?");
         scanf(" %c", &type);
         if(type == 'r'){
-            rec(s, val);
+            p.s = rec(p.s, val);
         } else if (type == 'd') {
-            dis(s, val);
+            p.s = dis(p.s, val);
         }
-        i += 1;
     }
     
-    printf("Numero do Projeto: %d", p.num);
-    printf("Saldo Inicial: %.2lf", p.s);
-    printf("Saldo Final: %.2lf", s);
+    printf("\nNumero do Projeto: %d", p.num);
+    printf("\nSaldo Inicial: R$%.2f", initial_s);
+    printf("\nSaldo Final: R$%.2f", p.s);
     
     return 0;
 }
