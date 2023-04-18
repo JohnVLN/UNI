7de97affc63c7e39ee2ed9285ca8c03509e86f4e
JoonathanVL <106608808+JohnVLN@users.noreply.github.com>
2023-04-18 19:38:29 -0300

    Delete Temp directory

--- Temp/tempcsv_creator.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open('temps.txt', 'r') as in_file:
-    stripped = (line.strip() for line in in_file)
-    lines = (line.split(" ") for line in stripped if line)
-    with open('coleta.csv', 'w', newline = '') as out_file:
-        writer = csv.writer(out_file, delimiter = ' ')
-        writer.writerows(lines)
