65544902fd8fb1dd6a0b74049ecc563d45cb6a14
JoonathanVL <106608808+JohnVLN@users.noreply.github.com>
2023-05-29 20:22:26 -0300

    Update RAD - Python & SQL Server - Menu.py

--- Python/RAD/RAD - Python & SQL Server - Menu.py
+++ Python/RAD/RAD - Python & SQL Server - Menu.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
 import pyodbc
 
 # Estabelecendo a conexão com a db
@@ -21,7 +15,8 @@ while i == 0:
     print("1. Consulta Prof")
     print("2. Consulta Dep")
     print("3. Cadastra Prof")
-    print("4. Exit...")
+    print("4. Relatório")
+    print("5. Exit...")
 
     # Cria cursor e a integer choice
     cursor = conn.cursor()
@@ -30,31 +25,41 @@ while i == 0:
     # Consulta Pofessor
     if choice == 1:
         cod_p = int(input("Insira o código do professor: "))
-        cursor.execute('SELECT * FROM professor WHERE Cod_Professor={cod_p}')
+        cursor.execute('SELECT * FROM Professor WHERE Cod_Professor=(?)', cod_p)
         for l in cursor:
             print(l)
+            
     # Consulta Departamento
     elif choice == 2:
         dep = int(input("Insira o código de departamento: "))
-        cursor.execute('SELECT * FROM departamento WHERE Cod_Departamento={cod_d}')
+        cursor.execute('SELECT * FROM Departamento WHERE Cod_Departamento=(?)', dep)
         for l in cursor:
             print(l)
+            
     # Cadastro Professor
     elif choice == 3:
         nom = input("Nome do professor a ser cadastrado: ")
         sob = input("Sobrenome: ")
         stat = input("Status (True/False): ")
         dep = input("Nome do Departamento: ")
-        cursor.execute('INSERT INTO professor(Nome_Professor, Sobrenome_Professor, Status, Cod_Departamento) VALUES ({nom}, {sob}, {stat}, (SELECT Cod_Departamento FROM Departamento WHERE Nome_Departamento={dep}))')  
+        cursor.execute('INSERT INTO Professor(Nome_Professor, Sobrenome_Professor, Status, Cod_Departamento) VALUES (?, ?, ?, (SELECT Cod_Departamento FROM Departamento WHERE Nome_Departamento=(?)))', nom, sob, stat, dep)  
         #commit insert
         conn.commit()
         print("Professor Cadastrado!")
-    #Exit programm
+        
+    # Cria relatório contendo toda informação dos professores
     elif choice == 4:
+        cursor.execute('SELECT * FROM Professor')
+        with open('relProfessores.txt', 'w') as f:
+            for l in cursor:
+                f.write(str(l))
+        print("Criado o relatório 'relProfessores'")
+        
+    #Exit programm
+    elif choice == 5:
         i = choice
     else:
         print("Escolha uma opção válida...")
-
+        
 cursor.close()
 conn.close()
-
